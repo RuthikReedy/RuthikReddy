9a3bafc41fb652efc8d45b7b7dfb64ad642bb318
RuthikReedy <ruthikreddy611@gmail.com>
2024-10-17 15:22:00 +0000

    container config done

--- basics/DTconversion.js
+++ basics/DTconversion.js
@@ -1,1 +1,14 @@
 console.log("heloo world");
+let score="33"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+
+let name="Ruthikreddy"
+let stringInto=Number(name)
+console.log(stringInto);
+//str convert into NaN (not a number but a number)
+
+let isLoggedIn=false
+let booleanInto=Number(typeof isLoggedIn)
+console.log(booleanInto);
